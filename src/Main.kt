96733ab1b19fe018abc7b3dce2c5dac421db1d03
alrweashd <trkyabler@gmail.com>
2025-02-12 20:28:24 +0300

    Done 1 to 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
     println("Hello World!")
+    greetSeaCreatures();
+    println("${findPearl(6)}")
+    println("${divideTreasure(1000.0,5)}")
+//    println(trainees)
+
 }
+val greetSeaCreatures = { println("Hello, Deep Sea Adventurer!") }
+
+val findPearl = {pearl: Int -> pearl * pearl}
+
+val divideTreasure: (Double, Int) -> Double = {totalTreasure, explorers ->
+    //to understand return in lambda
+    if (totalTreasure<0){
+        0.0
+    }
+    else {
+        totalTreasure / explorers
+    }
+}
+//val trainees = arrayListOf("aziz","aziz");
